40f0d6244c4fb9b22833625e625b6c51f05aec68
SundarJ <sundarjoshi@hotmail.co.uk>
2016-01-08 01:57:44 +0000

    remove routes dependency

--- index.js
+++ index.js
@@ -3,12 +3,6 @@
 const http = require('http')
 const fs = require('fs')
 const serve = require('serve-static')('exports')
-const router = require('routes')()
-
-router.addRoute('/favicon.ico', (req, res) => {
-    res.setHeader('content-type', 'image/x-icon')
-    return res.end()
-})
 
 const final = (req, res) => {
     return function() {
@@ -17,16 +11,19 @@ const final = (req, res) => {
     }
 }
 
-router.addRoute('*', (req, res) => {
-    return serve(req, res, final(req, res))
-})
+const favicon = (res) => {
+    res.setHeader('content-type', 'image/x-icon')
+    return res.end()
+}
 
 let server = http.createServer(
     (req, res) => {
-        let m = router.match(req.url)
-        
-        if (m)
-            return m.fn(req, res)
+        switch(req.url) {
+            case '/favicon.ico':
+                return favicon(res);
+            default:
+                return serve(req, res, final(req, res))
+        }
     }
 )
 server.listen( 8080, 
